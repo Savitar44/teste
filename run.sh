6a194e53725c508392b2d69d1cb748527e2a4464
The_Flash <90428057+Savitar44@users.noreply.github.com>
2021-09-30 06:28:18 -0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+echo -e "\033[0;31mDigite seu ip: \033[0m"
+read ip
+sleep 2s
+echo -e
+echo -e "\033[0;31mDigite sua porta: \033[0m"
+read port
+sleep 2s
+echo -e
+echo -e "\033[0;31mDigite seu usuário: \033[0m"
+read user
+sleep 2s
+clear
+echo -e "\033[0;32mIniciando Servidor SSH...\033[0m"
+sleep 3s
+clear
+ssh -p $port $user@ip
