27e3d81bba4b9c7d36ffaf458def80e39c700769
The_Flash <90428057+Savitar44@users.noreply.github.com>
2021-10-03 08:03:23 -0300

    Update and rename start to on.sh

--- on.sh
+++ on.sh
@@ -5,23 +5,6 @@ GREEN='\033[1;33m'
 
 clear
 read -p "$(echo -e $BLUE"Deseja instalar pacotes (s/n)? "$SC)" sn
-case $sn in
-   [Ss]*)
-   clear
-   echo -e "${GREEN}Instalando OPENSSH...${SC}"
-   echo -e
-   sleep 3s
-   pkg install openssh
-   clear
-   echo -e "${GREEN}Instalando DROPBEAR...${SC}"
-   echo -e
-   sleep 3s
-   pkg install dropbear
-   clear
-   ;;
-   [Nn]*)
-   clear
-   ;;
 read -p "Digite seu ip: " ip
 sleep 2s
 echo -e
