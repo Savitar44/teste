0fc97f76f045247a37249292f9674e782b6fc46c
The_Flash <90428057+Savitar44@users.noreply.github.com>
2021-10-02 21:30:30 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,27 +5,31 @@ GREEN='\033[0;32m'
 SC='\033[0m'
 
 clear
-echo -e "${BLUE}Digite seu ip: ${SC}"
-read ip
+read -p "$(echo -e $BLUE"Digite seu ip: "$SC)" ip
 sleep 2s
 echo -e
-echo -e "${BLUE}Digite sua porta: ${SC}"
-read port
+read -p "$(echo -e $BLUE"Digite sua porta: "$SC)" port
 sleep 2s
 echo -e
-echo -e "${BLUE}Digite seu usuário: ${SC}"
-read user
+read -p "$(echo -e $BLUE"Digite seu usuário: "$SC)" user
 sleep 2s
 clear
 echo -e "${GREEN}Iniciando Servidor SSH...${SC}"
-sleep 3s
-clear
+sleep 2s
 ssh -p $port $user@$ip
 clear
-echo -e "${GREEN}Parando Servidor...${SC}"
-sleep 2s
-clear
-echo -e "${RED}Retornando...${SC}"
-sleep 2s
-curl -sO https://raw.githubusercontent.com/Savitar44/teste/main/run.sh && chmod 777 run.sh && ./run.sh
-
+read -p "$(echo -e $RED"Deseja desconectar o Servidor SSH (s/n)? "$SC)" sn
+   case $sn in
+   [Ss]*)
+   clear
+   exit
+   ;;
+   [Nn]*)
+   curl -sO https://raw.githubusercontent.com/Savitar44/teste/main/run.sh && chmod 777 run.sh && ./run.sh
+   ;;
+   *) echo -e "${RED}Opção inválida! ${SC}"
+   echo -e
+   sleep 3s
+   echo -e "${RED}Retornando...${SC}"
+   sleep 3s
+   curl -sO https://raw.githubusercontent.com/Savitar44/teste/main/run.sh && chmod 777 run.sh && ./run.sh
